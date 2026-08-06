c17bfb3f4a296e7a18fd3517f9eac79cc697627c
Shreya Awadhiya <shreya.awadhiya16@gmail.com>
2026-08-06 23:41:00 +0530

    LongestPreSum Sol

--- 01-Array/LongestPrefixSum.java
+++ 01-Array/LongestPrefixSum.java
@@ -1,9 +1,8 @@
 import java.util.Arrays;
 
-public class LongestPrifixSum{
+public class LongestPrefixSum{
 
-   
-    public String longestCommonPrefix(String[] strs) {
+    public static String longestCommonPrefix(String[] strs) {
       
         Arrays.sort(strs);
       String s1 = strs[0];
@@ -12,7 +11,7 @@ public class LongestPrifixSum{
     int i = 0;
 
     while(i < s1.length() && i< s2.length()){
-        if(s1.charAt[i] == s2.charAt[i]){
+        if(s1.charAt(i) == s2.charAt(i)){
             i++;
         }else{
             break;
@@ -24,7 +23,6 @@ public class LongestPrifixSum{
 
     public static void main(String[] args) {
         String strs[] = {"flower","flow","flight"};
-     LongestCommonPrefix(strs);
-
+    System.out.println(longestCommonPrefix(strs));
     }
 }   
