cd54c7a1614bb67fb74c8d491cf71c2cc53e7c55
Shreya Awadhiya <shreya.awadhiya16@gmail.com>
2026-08-12 23:18:43 +0530

    SingleNum Soln

---
+++ SingleNum.java
@@ -0,0 +1,17 @@
+public class SingleNum {
+
+     public static int singleNumber(int[] nums) {
+      int idx = 0;
+      for(int i =0 ; i <nums.length ; i++){
+        idx = idx^nums[i];
+      }
+      return idx;
+    }
+
+
+    public static void main(String[] args) {
+        int nums[] = {2,2,1};
+        System.out.println(singleNumber(nums));
+    }
+    
+}
