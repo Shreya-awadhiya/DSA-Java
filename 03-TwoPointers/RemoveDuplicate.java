5f61ee5dbeb25c1b194d094c37da3168540f58b2
Shreya Awadhiya <shreya.awadhiya16@gmail.com>
2026-08-26 19:20:04 +0530

    Solution

---
+++ 03-TwoPointers/RemoveDuplicate.java
@@ -0,0 +1,23 @@
+public class RemoveDuplicte {
+  
+     public static int removeDuplicates(int[] nums) {
+    
+        if(nums.length == 0){
+            return 0;
+        }
+        int k = 1;
+        for(int i=1 ; i<nums.length; i++){
+            if(nums[i] != nums[i-1]){
+              nums[k] = nums[i];
+              k++;
+            }
+        }
+        return k;
+    }
+    public static void main(String[] args) {
+        int nums[] = {1,1,2};
+        int s = removeDuplicates(nums);
+        System.out.println(s);
+
+    }
+}
