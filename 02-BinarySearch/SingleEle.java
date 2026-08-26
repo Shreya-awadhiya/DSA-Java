c6e49202316b04437353ec4fdd1ffaa0586af439
Shreya Awadhiya <shreya.awadhiya16@gmail.com>
2026-08-26 19:27:09 +0530

    Solution

---
+++ 02-BinarySearch/SingleEle.java
@@ -0,0 +1,23 @@
+public class SingleEle {
+        
+    public static int singleNonDuplicate(int[] nums) {
+        int left = 0, right = nums.length - 1;
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (mid % 2 == 1) {
+                mid--;
+            }
+            if (nums[mid] != nums[mid + 1]) {
+                right = mid;
+            } else {
+                left = mid + 2;
+            }
+        }
+        return nums[left];
+    }
+      public static void main(String[] args) {
+        int nums[] = {1,1,2,3,3,4,4,8,8};
+        int s = singleNonDuplicate(nums);
+System.out.println(s);
+      }
+}
