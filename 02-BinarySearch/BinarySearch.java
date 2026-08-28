6ca63512a18b44c991fc6331bd805797b924a743
Shreya Awadhiya <shreya.awadhiya16@gmail.com>
2026-08-28 21:37:19 +0530

    Solution

---
+++ 02-BinarySearch/BinarySearch.java
@@ -0,0 +1,25 @@
+public class BinarySearch {
+    
+   public static int search(int[] nums, int target) {
+        int low = 0 , high = nums.length-1;
+
+        while(low<=high){
+             int mid = (low+high)/2;
+            if(nums[mid]==target){
+                return mid;
+            }
+            else if(nums[mid]<target){
+                low = mid +1;
+            }
+            else{
+                high = mid-1;
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+       int nums[] = {-1,0,3,5,9,12};
+       int s = search(nums, 9);
+       System.out.println(s); 
+    }
+}
