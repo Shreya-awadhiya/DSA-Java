33ad14251f734a4fd5c04f3579266a20ada98ecf
Shreya Awadhiya <shreya.awadhiya16@gmail.com>
2026-09-03 12:27:45 +0530

    Solution

---
+++ 01-Arrays/ConstructUnfPartityArrI.java
@@ -0,0 +1,17 @@
+
+import java.util.*;
+
+public class ConstructUnfPartityArrI {
+
+    
+class Solution {
+    public static boolean uniformArray(int[] nums1) {
+        return true;
+    }
+}
+    public static void main(String[] args) {
+        int[] nums1 = {1, 1, 1, 1};
+        System.out.println(Solution.uniformArray(nums1));
+    }
+
+}
