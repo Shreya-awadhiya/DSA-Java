516d7cdb188984861f64edef76af6d081a738ad9
Shreya Awadhiya <shreya.awadhiya16@gmail.com>
2026-09-04 20:44:19 +0530

    SOLUTION

--- 01-Arrays/firstmissingpos.java
+++ 01-Arrays/firstmissingpos.java
@@ -1,24 +1,23 @@
+import java.util.*;
+
 public class firstmissingpos {
-    public static int firstMissingPositive(int[] nums) {
-        int n = nums.length;
-        for (int i = 0; i < n; i++) {
-            while (nums[i] > 0 && nums[i] <= n && nums[nums[i] - 1] != nums[i]) {
-                int temp = nums[nums[i] - 1];
-                nums[nums[i] - 1] = nums[i];
-                nums[i] = temp;
+    
+        public int firstMissingPositive(int[] nums) {
+        int max = Integer.MIN_VALUE; //-1
+
+        Arrays.sort(nums); // 0,1,3
+        for(int i =0; i<nums.length ;i++){
+            if(nums[i] < max){
+                max = nums[i];
             }
         }
-        for (int i = 0; i < n; i++) {
-            if (nums[i] != i + 1) {
-                return i + 1;
-            }
-        }
-        return n + 1;
+        return max + 1;
     }
 
     public static void main(String[] args) {
         int[] nums = {3, 4, -1, 1};
-        System.out.println(firstMissingPositive(nums)); // Output: 2
+        firstmissingpos solution = new firstmissingpos();
+        System.out.println(solution.firstMissingPositive(nums)); // Output: 2
     }
 
 }
