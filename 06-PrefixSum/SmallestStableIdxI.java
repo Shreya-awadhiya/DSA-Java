e9bdb335b82cb37524224055517b8b1987c74dba
Shreya Awadhiya <shreya.awadhiya16@gmail.com>
2026-09-05 11:14:15 +0530

    Solution

---
+++ 06-PrefixSum/SmallestStableIdxI.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+public class SmallestStableIdxI {
+ 
+        public static int firstStableIndex(int[] nums, int k) {
+        int n = nums.length;
+
+        int[] suffixMin = new int[n];
+
+        suffixMin[n - 1] = nums[n - 1];
+
+        for (int i = n - 2; i >= 0; i--) {
+            suffixMin[i] = Math.min(nums[i], suffixMin[i + 1]);
+        }
+
+        int prefixMax = nums[0];
+
+        for (int i = 0; i < n; i++) {
+            prefixMax = Math.max(prefixMax, nums[i]);
+
+            int instability = prefixMax - suffixMin[i];
+
+            if (instability <= k) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+    
+    public static void main(String[] args) {
+        int nums[] = {5,0,1,4};
+        int k = 3;
+        System.out.println(firstStableIndex(nums, k));
+    }
+}
