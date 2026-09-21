e615013852bab35572f096728c562bd5ee614c85
Shreya Awadhiya <shreya.awadhiya16@gmail.com>
2026-09-21 13:00:09 +0530

    Solution

---
+++ 11-Recursion/Fibonacci.java
@@ -0,0 +1,20 @@
+import java.util.*; 
+public class Fibonacci{
+
+
+        public static int fib(int n) {
+        if(n == 0 || n ==1){
+            return n;
+        }
+
+        int f1 = fib(n-1);
+        int f2 = fib(n-2);
+
+        return f1+f2;
+    }
+    public static void main(String[] args) {
+        int n = 2;
+        System.out.println(fib(n));
+
+    }
+}
